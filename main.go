95cf724f7108e5f7b42ab078b6a3357b143d9e7c
Reed O'Brien <reed@reedobrien.com>
2015-01-24 14:29:19 -0500

    Empty main with placeholder for output.

--- main.go
+++ main.go
@@ -2,3 +2,12 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("code appears here")
+}
